8edadb2223ea32ab93ae963b5c34318c413d10d5
Seth Reid <seth.reid@xero.com>
2021-12-04 13:09:57 +1300

    init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,59 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import App from "./App";
+import { getAll } from "./api/users";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("./api/users", () => {
+  return {
+    getAll: jest.fn(),
+  };
+});
+
+describe("<App />", () => {
+  const mockGetAllUsers = getAll as jest.MockedFunction<typeof getAll>;
+  beforeEach(() => {
+    mockGetAllUsers.mockReset();
+  });
+
+  it("should show title and list of users", async () => {
+    // arrange
+    const users = [
+      {
+        id: "1",
+        name: "chuck norris",
+        email: "email@chucknorris.com",
+      },
+    ];
+    mockGetAllUsers.mockResolvedValue(users);
+
+    // act
+    render(<App />);
+
+    // assert
+    await waitForElementToBeRemoved(screen.getByText("Loading"));
+    expect(screen.getByRole("heading", { name: "Users" })).toBeInTheDocument();
+
+    for (let index = 0; index < users.length; index++) {
+      const user = users[index];
+      expect(screen.getByText(user.name)).toBeInTheDocument();
+    }
+  });
+
+  it("should show error when user api request fails", async () => {
+    // arrange
+    const errorMessage = "it's fucked";
+    mockGetAllUsers.mockRejectedValue(errorMessage);
+
+    // act
+    render(<App />);
+
+    // assert
+    await waitForElementToBeRemoved(screen.getByText("Loading"));
+    expect(screen.getByRole("heading", { name: "Users" })).toBeInTheDocument();
+
+    expect(screen.getByText(errorMessage)).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { getAll, User } from "./api/users";
+type UserState = {
+  value: User[];
+  status: Status;
+  error?: string;
+};
+
+enum Status {
+  Idle,
+  Loading,
+  Complete,
+  Failed,
+}
 
 function App() {
+  const [userState, setUserState] = useState<UserState>({
+    value: [],
+    status: Status.Idle,
+  });
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        // could do ...userState
+        setUserState({ value: [], status: Status.Loading });
+        const users = await getAll();
+        setUserState({ value: users, status: Status.Complete });
+      } catch (error) {
+        setUserState({
+          value: [],
+          status: Status.Failed,
+          error: (error as Error).message,
+        });
+      }
+    };
+
+    if (userState.status === Status.Idle) {
+      fetchUsers();
+    }
+  }, [userState]);
+
+  const renderContent = () => {
+    if (userState.status === Status.Failed) {
+      return <p>it's fucked</p>;
+    }
+
+    return (
+      <ul>
+        {userState.value.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    );
+  };
+
+  if (userState.status === Status.Idle || userState.status === Status.Loading) {
+    return <p>Loading</p>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Users</h1>
+
+      {renderContent()}
     </div>
   );
 }
